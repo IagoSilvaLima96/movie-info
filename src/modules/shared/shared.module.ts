35e3f170f13889ab09c714a4844193e46dfb09b6
Iago Silva Lima <iagocrouchlima@gmail.com>
2024-07-14 14:41:19 -0300

    feat: adiciona shared module e servico http

---
+++ src/modules/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { HttpModule } from '@nestjs/axios';
+import { Global, Module } from '@nestjs/common';
+import { HttpClientService } from './services/http-client/http-client.service';
+
+@Global()
+@Module({
+  imports: [HttpModule],
+  providers: [HttpClientService],
+  exports: [HttpClientService],
+})
+export class SharedModule {}
